88252f11db58203279c79760805f53567730bf4a
Atanael Mendes <atanael_snp@hotmail.com>
2019-06-21 21:14:48 -0400

    [ADD] criado a classe page

--- index.php
+++ index.php
@@ -1,21 +1,22 @@
 <?php
 
-require_once("vendor/autoload.php");
+	require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+	use \Slim\Slim;
+	use  \Hcode\Page;
 
-$app->config('debug', true);
+	$app = new Slim();
 
-$app->get('/', function() {
+	$app->config('debug', true);
 
-	$sql = new Hcode\DB\Sql();
+	$app->get('/', function() {
 
-  $qry = $sql->select("SELECT * FROM tb_users");
+		$page = new Page();
 
-  echo json_encode($qry);
+		$page->setTpl("index");
 
-});
+	});
 
-$app->run();
+	$app->run();
 
- ?>
+?>
